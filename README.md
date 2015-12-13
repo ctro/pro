97206893dbd28a0ae333d43d3ddc1079fcdac8e1
ctro <clint@ctro.net>
2015-12-13 15:30:38 -0700

    Simplifying things for a new install. Also, what happened to uTorrent!?

--- README.md
+++ README.md
@@ -9,10 +9,3 @@ Run 'em
 ```sh
 cd pro && sh pro.sh
 ```
-
-TODO:
------
-- scripted three finger swipe back
-- get seil to correctly load capslock override
-- setting default finder location doesn't work
-- you still have to set up ssh keys manually
--- pro.sh
+++ pro.sh
@@ -1,78 +1,37 @@
 #!/usr/bin/env bash
 
-# just ask for root...
-sudo -v
-
 echo "Installing Homebrew..."
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Checking Homebrew..."
 brew doctor
-echo "Updating Homebrew..."
 brew update
 
-echo "Installing Ruby..."
-RV="2.1.4"
-brew install rbenv ruby-build
-rbenv install $RV
-rbenv global $RV
-
 echo "Installing brews..."
+brew install ruby
+brew install docker
 brew install postgresql
-brew install heroku-toolbelt
 
 echo "Installing casks..."
-brew install caskroom/cask/brew-cask
-brew cask install onepassword
-brew cask install dropbox
-brew cask install evernote
+brew cask install iterm2
 brew cask install google-chrome
+brew cask install lastpass
+brew cask install google-drive
 brew cask install atom
-#brew cask install alfred
-brew cask install limechat
-brew cask install torbrowser
-brew cask install gitx
-brew install libdvdcss
-brew cask install handbrake
-brew cask install fantastical
-brew cask install utorrent
 brew cask install spectacle
-brew cask install rdio
-brew cask install mailbox
 brew cask install caffeine
-brew cask install licecap
-brew cask install google-earth
-
-echo "Configuring Terminal..."
-# Only use UTF-8 in Terminal.app
-defaults write com.apple.terminal StringEncodings -array 4
-brew install bash-completion
-
-#Solarized themes
-git clone git://github.com/tomislav/osx-lion-terminal.app-colors-solarized.git ~/.solarized-terminal
-open ~/.solarized-terminal/Solarized\ Light.terminal
-open ~/.solarized-terminal/Solarized\ Dark.terminal
-sleep 1 # Wait a bit to make sure the theme is loaded
-defaults write com.apple.terminal "Default Window Settings" -string "Solarized Dark"
-defaults write com.apple.terminal "Startup Window Settings" -string "Solarized Dark"
+brew cask install seil   # must manually set this up, i.e. CAPS->esc
 
 echo "Configuring Gems..."
 gem update –system
 gem install bundler
 
-echo "Installing keyboard bindings..."
-brew cask install seil
-#sh ./lib/seil-import.sh  <-- doesn't work for caps lock
-
-echo "Configuring OSX..."
-sh ./lib/ctro_osx.sh
+#echo "Configuring OSX..."
+#sh ./lib/ctro_osx.sh
 
 echo "Configuring ENV..."
 cp ./static/vimrc ~/.vimrc
-cat /usr/share/vim/vim73/vimrc_example.vim >> ~/.vimrc
 cp ./static/bash_profile ~/.bash_profile
 cp ./static/gitconfig ~/.gitconfig
 cp ./static/gemrc ~/.gemrc
-
-echo "Run Installers..."
-open /opt/homebrew-cask/Caskroom/utorrent/latest/uTorrent-Installer.app
 
 #reboot
